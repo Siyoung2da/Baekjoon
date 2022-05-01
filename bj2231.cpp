c7b942be1a7fb8d348373b890916b4c0216dc8ae
Siyoung Park <drq5@daum.net>
2022-05-01 16:19:19 +0900

    ~220501

--- bj2231.cpp
+++ bj2231.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-    int N;
+    int N, sum, count;
     cin >> N;
+    sum = 0;
+    count = 0;
+    for (int i=0;i<N;i++) {
+        sum = i;
+        int j = i;
+        while (j!=0) {
+            sum += j%10;    
+            j = j/10;
+        }
+        if (N == sum) {
+            cout << i << endl;
+            count++;
+            break;
+        }
+    }
+    if (count==0) cout << "0" << endl;
 }
