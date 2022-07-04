9722e6523545d090d140bb4d09dabf0522ab37e6
Siyoung Park <drq5@daum.net>
2022-07-04 15:22:28 +0900

    ~220704

--- algorithm_study/week1/data_structure/bj1966.cpp
+++ algorithm_study/week1/data_structure/bj1966.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
 
-queue<int> printer;
-
-int main() {
+int main()
+{
     int T;
     cin >> T;
-    for (int t=0;t<T;t++) {
+    for (int t = 0; t < T; t++)
+    {
+        vector<pair<int, int> > printer;
         int n, m;
         cin >> n >> m;
-        for(int i=0;i<n;i++) {
+        for (int i = 0; i < n; i++)
+        {
             int importance;
             cin >> importance;
-            printer.push(importance);
+            printer.push_back(pair<int, int>(i, importance));
         }
-        while(1) {
-            if(printer.front())
-            
+        int cnt = 0;
+        while(1)
+        {
+            int k = printer[0].first;
+            int check = 0;
+            for (int i = 1; i < printer.size(); i++)
+            {
+                if (printer[i].second > printer[0].second)
+                {
+                    check++;
+                    printer.push_back(printer[0]);
+                    printer.erase(printer.begin());
+                    break;
+                }
+            }
+            if(check==0)
+            {
+                if (k == m) {
+                    printer.erase(printer.begin());
+                    cnt++;
+                    break;
+                }
+                printer.erase(printer.begin());
+                cnt++;
+            }
         }
+        cout << cnt << "\n";
     }
 }
