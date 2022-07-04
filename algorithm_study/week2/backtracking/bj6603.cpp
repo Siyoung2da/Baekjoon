b0f435c3cf6ad6e670dca0d51a0dc4fde560fe83
Siyoung Park <drq5@daum.net>
2022-07-04 18:01:38 +0900

    ~220704

--- algorithm_study/week2/backtracking/bj6603.cpp
+++ algorithm_study/week2/backtracking/bj6603.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     while (1) {
         int k;
         cin >> k;
         if (k==0) break;
 
+        vector<int> check;
         vector<int> lotto;
 
         for (int i=0;i<k;i++) {
@@ -15,5 +21,21 @@ int main() {
             cin >> n;
             lotto.push_back(n);
         }
+        for(int i=0;i<k;i++) {
+            if(i<6) {
+                check.push_back(0);
+            }
+            else 
+                check.push_back(1);
+        }
+        do {
+            for (int i=0;i<k;i++) {
+                if(check[i]==0) {
+                    cout << lotto[i] << " ";
+                }
+            }
+            cout << "\n";
+        }while(next_permutation(check.begin(), check.begin() + k));
+        cout << "\n";
     }
 }
